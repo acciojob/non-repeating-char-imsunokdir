5802eaa2747441fb3b3d7342db9c03c60f576c80
imsunokdir <67774734+imsunokdir@users.noreply.github.com>
2024-07-15 14:21:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let strCount= {}
+	for(let i of str){
+		strCount[i] = (strCount[i] || 0) + 1
+	}
+	for(let i of str){
+		if(strCount[i] === 1){
+			return i
+		}
+	} 
+	return null
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
